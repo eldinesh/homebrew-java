d29ead191955f409e058df796e17fcdfcfe95550
Dinesh Kumar <dseera6@gmail.com>
2022-03-11 11:03:04 +0530

    sha commit

--- mysql-connector-java.rb
+++ mysql-connector-java.rb
@@ -4,7 +4,7 @@ class MysqlConnectorJava < Formula
   homepage "http://dev.mysql.com/downloads/connector/j/"
   version "8.0.28"
   url "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-#{version}.tar.gz"
-  sha256 "4d261f669f685cd9e1eb5683d03019466cb9e59a8b873efce1b623254ea29231"
+  sha256 "859afba47f1fd99eb7c010bfa101ed7c0f8bb278f6f62430adc8012ea82fb454"
 
   def install
     prefix.install_metafiles
